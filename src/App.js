2b6e421ea4aa79204e2922594496b686f0d0b02f
miaochuanwei <miaochw@inspur.com>
2018-10-03 10:16:38 +0800

    m

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Layout from './pages/Layout';
-import routes from './routes';
-import PageHeader from './component/PageHeader';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div style={{ height: '100%' }}>
-        <Layout style={{ height: '100%' }}>
-          <Switch>
-            {routes.map(route => (
-              <Route
-                {...route}
-                key={route.name}
-                render={props => (route.loadComponent ? (
-                  <PageHeader {...props} name={route.name}>
-                    {React.createElement(route.loadComponent, props)}
-                  </PageHeader>
-                ) : null)
-                }
-              />
-            ))}
-          </Switch>
-        </Layout>
-      </div>
-    </BrowserRouter>
-  );
+  return <h1>Index</h1>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { LocaleProvider } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
 import App from './App';
 
-ReactDOM.render(
-  <LocaleProvider locale={zhCN}>
-    <App />
-  </LocaleProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
